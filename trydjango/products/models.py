701ca8cc63f7a52dc9bbe38e84c3343bba4ce3c4
Shihab Uddin Piash <Piash222@gmail.com>
2019-04-04 13:37:51 +0600

    initial commit

---
+++ trydjango/products/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+# Create your models here.
+class Products(models.Model):
+    title = models.CharField(max_length=120)
+    description = models.TextField(blank=True, null = True)
+    price = models.DecimalField(max_digits=100, decimal_places=2)
+    summery = models.TextField(blank = False, null=False)
+    featured = models.BooleanField()
